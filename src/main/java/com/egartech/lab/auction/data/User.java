a1501b15331fabda6221d183a107e430be0f6b6b
MaxBondarev <bondarev1234@gmail.com>
2017-07-13 15:52:41 +0400

    Jsp + Servlets + FrontController + Hibernate

--- src/main/java/com/egartech/lab/auction/data/User.java
+++ src/main/java/com/egartech/lab/auction/data/User.java
@@ -2,12 +2,24 @@ package com.egartech.lab.auction.data;
 
 
 public class User {
+
+
+    private Integer id;
     private String login;
     private String password;
 
     public User(String login, String password) {
         setLogin(login);
         setPassword(password);
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getLogin() {
